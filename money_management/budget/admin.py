ce156b15c7729a43f1ef5f2fb84c2cf77248a8ec
Josh Rabideaux <>
2021-06-04 10:23:00 -0500

    Added the model for a user to create there own categories

--- money_management/budget/admin.py
+++ money_management/budget/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import UserCategory
 
 # Register your models here.
+
+admin.site.register(UserCategory)
--- money_management/budget/models.py
+++ money_management/budget/models.py
@@ -1,3 +1,9 @@
+from django.conf import settings
 from django.db import models
+import uuid
 
 # Create your models here.
+class UserCategory(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    category = models.CharField(max_length=32)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
