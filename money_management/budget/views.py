40cc15dc93360951b9e0df494aad01a1de26eacd
Josh Rabideaux <>
2021-06-07 10:03:59 -0500

    Added login

--- money_management/budget/views.py
+++ money_management/budget/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 from .models import UserPlanItem
 from datetime import datetime
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def get_expense_plan_items_current_month(request):
@@ -14,3 +16,12 @@ def get_expense_plan_items_current_month(request):
     else:
         raise(PermissionError)
     return HttpResponse(plan_item_list)
+
+def login_view(request):
+    template = loader.get_template("login.html")
+    username = request.POST('username')
+    password = request.POST('password')
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+    return render(request, '/login.html')
--- money_management/money_management/urls.py
+++ money_management/money_management/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from budget import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dashboard', views.get_expense_plan_items_current_month)
+    path('dashboard', views.get_expense_plan_items_current_month),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
